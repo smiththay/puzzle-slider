2f81405693939c841e3cb3cc5b9c8342df5d975c
Smith Thay <contact.smith.thay@gmail.com>
2021-04-03 13:23:36 -0400

    grid is rendered

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import Tile from './Tile'
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={ 
+      board: [],
+      winner: false,
+    }
+  }
+
+  componentDidMount(){
+
+    let newBoard = [];
+    for(let i = 0; i < 16; i++ ){
+      newBoard.push({
+        id : i,
+        clicked: false,
+        number: i
+      })
+   
+  }
+    this.setState({ board: newBoard});
+    console.log(this.state.board)
+  } 
+
+
+  render() {
+    return (
+
+     <div className = "App">
+     <h1> Puzzle Slider </h1>
+     <div className='container'>
+     <div className="row">
+     {this.state.board.map ((tile, index) => <Tile  index={index} id={tile.id} /> )}
+     </div>
+      </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
+
---
+++ src/Tile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function tile(props) {
+    
+    
+    return (
+        <div className="col-3 border">
+
+
+         {props.id}  
+
+        </div>
+    )
+}
